d1cadbe61983e7fb3a0ba384f76f92b338a05721
Chris Read <ChrisR@intergen.org.nz>
2018-05-24 11:39:48 +1200

    started developing playerController

--- Assets/Scripts/Player/InputController.cs
+++ Assets/Scripts/Player/InputController.cs
@@ -2,15 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputController : MonoBehaviour {
+public class InputController : MonoBehaviour
+{
+    [SerializeField] private Motor playerMotor;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [Header("Control Settings")]
+    [SerializeField]
+    private string xMovAxis = "Horizontal";
+    [SerializeField]
+    private string zMovAxis = "Vertical";
+    [SerializeField]
+    private string xLookAxis = "LookX";
+    [SerializeField]
+    private string yLookAxis = "LookY";
+    [SerializeField]
+    private string jumpButton = "Jump";
+    [SerializeField]
+    private string crouchButton = "Crouch";
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get vertical and horizontal input vectors
+        Vector3 movement = new Vector3(Input.GetAxisRaw(xMovAxis), 0f, Input.GetAxisRaw(zMovAxis));
+        playerMotor.Move(movement);
+
+        // Get vertical and horizontal camera movement
+        float xMov = Input.GetAxisRaw(xLookAxis) * Time.deltaTime;
+        float yMov = Input.GetAxisRaw(yLookAxis) * Time.deltaTime;
+        playerMotor.MoveCamera(xMov, yMov);
+
+        // Check for Jump
+        if (Input.GetButtonDown(jumpButton))
+            playerMotor.Jump(true);
+
+        if (Input.GetButtonUp(jumpButton))
+            playerMotor.Jump(false);
+
+        // Check for Crouch
+        if (Input.GetButtonDown(crouchButton))
+            playerMotor.Crouch(true);
+
+        if (Input.GetButtonUp(crouchButton))
+            playerMotor.Crouch(false);
+    }
 }
---
+++ Assets/Scripts/Player/Motor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public abstract class Motor : MonoBehaviour
+{
+    public abstract void Move(Vector3 input);
+
+    public abstract void MoveCamera(float xMov, float yMov);
+
+    public abstract void Jump(bool pressed);
+
+    public abstract void Crouch(bool pressed);
+}

aac1cc7bf729f025bb68c2e677bfd0a72ad3a7ad
Chris Read <ChrisR@intergen.org.nz>
2018-05-29 16:46:19 +1200

    added basic camera follow script

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private bool rotateToFace;
+
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start()
+    {
+        transform.LookAt(target);
+        offset = transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rotateToFace)
+            transform.LookAt(target);
+        else
+            transform.position = target.position + offset;
+    }
+}

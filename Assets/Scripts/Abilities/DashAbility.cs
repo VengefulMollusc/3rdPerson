25c4940542b1d27a9f6c105563f55313eaa5ea6a
Chris Read <ChrisR@intergen.org.nz>
2018-06-12 11:14:46 +1200

    restructured script folders, added lightmech motor and began dashability

---
+++ Assets/Scripts/Abilities/DashAbility.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashAbility : Ability
+{
+    private Motor motor;
+
+    void Start()
+    {
+        motor = GetComponent<Motor>();
+    }
+
+    public override void Activate(bool activated)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/Motors/LightMechMotor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class LightMechMotor : Motor
+{
+    public override void Move(Vector2 input)
+    {
+        Vector3 movementDirection = new Vector3(input.x, 0f, input.y) * baseMoveSpeed * speedModifier * Time.deltaTime;
+        transform.position += movementDirection;
+        transform.rotation = Quaternion.LookRotation(movementDirection);
+    }
+}
